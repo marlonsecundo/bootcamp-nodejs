e77482867a24ad63a5b66c65ff28469a4d59f5f1
Marlon Secundo <marlon_secundo@outlook.com>
2018-11-28 21:13:27 -0300

    Added Sequelize

---
+++ module2/src/config/databse.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '127.0.0.1',
+  username: 'docker',
+  password: 'docker',
+  database: 'gonodemodulo2',
+  operatirAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
