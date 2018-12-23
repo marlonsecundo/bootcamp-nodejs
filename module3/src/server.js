da18f67a2e1ac8d3f2a654e511fd48acd0edd0f8
Marlon Secundo <marlon_secundo@outlook.com>
2018-12-23 13:42:46 -0300

    Module 3 - Start Project

---
+++ module3/src/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const databaseConfig = require('./config/database');
+
+class App {
+  constructor() {
+    this.express = express();
+    this.isDev = process.env.NODE_ENV !== 'production';
+  }
+
+  database() {
+    mongoose.connect(
+      databaseConfig.uri,
+      {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+      },
+    );
+  }
+
+  middlewares() {
+    this.express.use(express.json());
+  }
+
+  routes() {
+    this.express.use(require('./routes'));
+  }
+}
+
+module.exports = new App().express;
