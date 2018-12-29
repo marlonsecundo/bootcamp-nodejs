cd41d0253d475f1dea9cfbe8e94fa78c96faee93
Marlon Secundo <marlon_secundo@outlook.com>
2018-12-29 11:32:27 -0300

    Validators

--- module3/src/routes.js
+++ module3/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const validate = require('express-validation');
 
 const routes = express.Router();
 
@@ -10,20 +11,21 @@ const {
   AdController,
   PurchaseController,
 } = require('./app/controllers');
+const validators = require('./app/validators');
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/users', validate(validators.User), UserController.store);
+routes.post('/sessions', validate(validators.Session), SessionController.store);
 
 routes.use(authMiddleware);
 
 // Ads
 routes.get('/ads', AdController.index);
 routes.get('/ads/:id', AdController.show);
-routes.post('/ads', AdController.store);
-routes.put('/ads/:id', AdController.update);
+routes.post('/ads', validate(validators.Ad), AdController.store);
+routes.put('/ads/:id', validate(validators.Ad), AdController.update);
 routes.get('/ads/:id', AdController.destroy);
 
 // Purchase
-routes.post('/purchases', PurchaseController.store);
+routes.post('/purchases', validate(validators.Purchase), PurchaseController.store);
 
 module.exports = routes;
