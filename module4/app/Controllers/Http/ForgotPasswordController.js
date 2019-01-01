57b13bfc19aedfcd33d9183c996967b4c375e351
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-01 14:17:49 -0300

    Mail

--- module4/app/Controllers/Http/ForgotPasswordController.js
+++ module4/app/Controllers/Http/ForgotPasswordController.js
@@ -1,4 +1,5 @@
 const User = use('App/Models/User');
+const Mail = use('Mail');
 const crypto = require('crypto');
 
 class ForgotPasswordController {
@@ -11,8 +12,19 @@ class ForgotPasswordController {
       user.token_created_at = new Date();
 
       await user.save();
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        (message) => {
+          message
+            .to(user.email)
+            .from('diego@email.com', 'Diego')
+            .subject('Recuperação de Senha');
+        },
+      );
     } catch (err) {
-      return response.status(err.status).send({ error: { message: 'Essa email existe?' } });
+      return response.status(err.status).send(err.stack);
     }
 
     return response.status(200).send();
--- module4/start/app.js
+++ module4/start/app.js
@@ -1,45 +1,29 @@
-"use strict";
-
 /*
 |--------------------------------------------------------------------------
 | Providers
 |--------------------------------------------------------------------------
-|
-| Providers are building blocks for your Adonis app. Anytime you install
-| a new Adonis specific package, chances are you will register the
-| provider here.
-|
 */
 const providers = [
-  "@adonisjs/framework/providers/AppProvider",
-  "@adonisjs/auth/providers/AuthProvider",
-  "@adonisjs/bodyparser/providers/BodyParserProvider",
-  "@adonisjs/cors/providers/CorsProvider",
-  "@adonisjs/lucid/providers/LucidProvider"
+  '@adonisjs/framework/providers/AppProvider',
+  '@adonisjs/auth/providers/AuthProvider',
+  '@adonisjs/bodyparser/providers/BodyParserProvider',
+  '@adonisjs/cors/providers/CorsProvider',
+  '@adonisjs/lucid/providers/LucidProvider',
+  '@adonisjs/mail/providers/MailProvider',
+  '@adonisjs/framework/providers/ViewProvider',
 ];
 
 /*
 |--------------------------------------------------------------------------
 | Ace Providers
 |--------------------------------------------------------------------------
-|
-| Ace providers are required only when running ace commands. For example
-| Providers for migrations, tests etc.
-|
 */
-const aceProviders = ["@adonisjs/lucid/providers/MigrationsProvider"];
+const aceProviders = ['@adonisjs/lucid/providers/MigrationsProvider'];
 
 /*
 |--------------------------------------------------------------------------
 | Aliases
 |--------------------------------------------------------------------------
-|
-| Aliases are short unique names for IoC container bindings. You are free
-| to create your own aliases.
-|
-| For example:
-|   { Route: 'Adonis/Src/Route' }
-|
 */
 const aliases = {};
 
@@ -53,4 +37,9 @@ const aliases = {};
 */
 const commands = [];
 
-module.exports = { providers, aceProviders, aliases, commands };
+module.exports = {
+  providers,
+  aceProviders,
+  aliases,
+  commands,
+};
