92ce2214417be3e480a130528d351c39d7dcad13
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-03 09:22:04 -0300

    task and project models

---
+++ module4/database/migrations/1546517564596_project_schema.js
@@ -0,0 +1,25 @@
+const Schema = use('Schema');
+
+class ProjectSchema extends Schema {
+  up() {
+    this.create('projects', (table) => {
+      table.increments();
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('SET NULL');
+      table.string('title').notNullable();
+      table.text('description').notNullable();
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('projects');
+  }
+}
+
+module.exports = ProjectSchema;
