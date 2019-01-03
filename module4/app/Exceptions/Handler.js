ad9be2e2c54938e90ca176d62a15f996501f3533
Marlon Secundo <marlon_secundo@outlook.com>
2019-01-03 13:49:40 -0300

    Exceptions

---
+++ module4/app/Exceptions/Handler.js
@@ -0,0 +1,23 @@
+const BaseExceptionHandler = use('BaseExceptionHandler');
+const Env = use('Env');
+const Youch = use('Youch');
+class ExceptionHandler extends BaseExceptionHandler {
+  async handle(error, { request, response }) {
+    if (error.name === 'ValidatonExcecption') {
+      return response.status(error.status).send(error.messages);
+    }
+
+    if (Env.get('NODE_ENV') === 'development') {
+      const youch = new Youch(error, request.request);
+      const errorJSON = await youch.toJSON();
+
+      return response.status(error.status).send(errorJSON);
+    }
+
+    return response.status(error.status);
+  }
+
+  async report(error, { request }) {}
+}
+
+module.exports = ExceptionHandler;
